3b238935c2fbb7d87ef14c6d6facb347be6ae134
dempseyucla <dempseyucla@gmail.com>
2026-01-21 21:30:44 -0800

    branch

--- A15.c
+++ A15.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 int one();
+int three();
+int two();
+int four();
 
 int main()
 {
     printf("main\n");
     one();
+    two();
+    three();
+    four();
 }
--- one.c
+++ one.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+int oneone();
+
 int one()
 {
     printf("one\n");
+    printf("one from github.com\n");
+    oneone();
 }
